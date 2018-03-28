a55a4d2fcc447dd36406fff1a99c6dc383282bcb
Quan <hokagequan@me.com>
2018-03-28 23:29:42 +0800

    base

---
+++ src/plus_minus.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+def plus_minus(arr):
+	count = len(arr)
+	
+
+if __name__ == '__main__':
+	n = int(input())
+	arr = list(map(int, input().rstrip().split()))
+	result = plus_minus(arr)
+	print(result)
