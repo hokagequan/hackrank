7bfd5633d5ae580c0d004ca3b185ce912406fcf6
Quan <hokagequan@me.com>
2018-04-05 22:26:41 +0800

    plus_minus

--- src/plus_minus.py
+++ src/plus_minus.py
@@ -3,10 +3,15 @@ import sys
 
 def plus_minus(arr):
 	count = len(arr)
-	
+	positve, negative, zero = 0, 0, 0
+	for x in arr:
+		positve += (1 if x > 0 else 0)
+		negative += (1 if x < 0 else 0)
+		zero += (1 if x == 0 else 0)
+	return (positve/count, negative/count, zero/count)
+
 
 if __name__ == '__main__':
-	n = int(input())
-	arr = list(map(int, input().rstrip().split()))
+	arr = [-4, 3, -9, 0, 4, 1]
 	result = plus_minus(arr)
 	print(result)
