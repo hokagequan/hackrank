4e04e2043e01d561939e0a47076b6d02e3714d54
QiM <hokagequan@me.com>
2018-04-18 15:29:43 +0800

    mini_max_sum

---
+++ src/max_min_sum.py
@@ -0,0 +1,21 @@
+import sys
+
+def minimaxsum(arr):
+	if len(arr) <= 1:
+		return
+
+	min_value = sys.maxsize
+	max_value = 0
+	for x in range(0, len(arr)):
+		tmp = arr[:]
+		del tmp[x]
+		sum_value = sum(tmp)
+		min_value = sum_value if sum_value < min_value else min_value
+		max_value = sum_value if sum_value > max_value else max_value
+
+	print("{} {}".format(min_value, max_value))
+
+
+if __name__ == '__main__':
+	arr = list(map(int, input().rstrip().split()))
+	minimaxsum(arr)
